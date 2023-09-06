26dc0b06e89ab54c2ee366f07d90ff76c944a202
reyngallego <mineminem6@gmail.com>
2023-09-07 02:30:47 +0800

    first commit

---
+++ kioskfinal/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace kioskfinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.purposetxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnok = new System.Windows.Forms.Button();
+            this.departmentcbx = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(390, 101);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(100, 20);
+            this.nametxt.TabIndex = 0;
+            // 
+            // purposetxt
+            // 
+            this.purposetxt.Location = new System.Drawing.Point(390, 191);
+            this.purposetxt.Name = "purposetxt";
+            this.purposetxt.Size = new System.Drawing.Size(100, 20);
+            this.purposetxt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "department:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "purpose:";
+            // 
+            // btnok
+            // 
+            this.btnok.Location = new System.Drawing.Point(308, 267);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(75, 23);
+            this.btnok.TabIndex = 6;
+            this.btnok.Text = "ok";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // departmentcbx
+            // 
+            this.departmentcbx.FormattingEnabled = true;
+            this.departmentcbx.Items.AddRange(new object[] {
+            "studentaffairs",
+            "department",
+            "registrar",
+            "director"});
+            this.departmentcbx.Location = new System.Drawing.Point(390, 148);
+            this.departmentcbx.Name = "departmentcbx";
+            this.departmentcbx.Size = new System.Drawing.Size(106, 21);
+            this.departmentcbx.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 379);
+            this.Controls.Add(this.departmentcbx);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.purposetxt);
+            this.Controls.Add(this.nametxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.TextBox purposetxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnok;
+        private System.Windows.Forms.ComboBox departmentcbx;
+    }
+}
+
---
+++ kioskfinal/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace kioskfinal
+{
+
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            // Check if any of the text boxes are empty
+            if (string.IsNullOrWhiteSpace(nametxt.Text) || string.IsNullOrWhiteSpace(departmentcbx.Text) || string.IsNullOrWhiteSpace(purposetxt.Text))
+            {
+                MessageBox.Show("Please fill in all fields before queuing.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Exit the method, don't proceed with the database insert
+            }
+
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+            string queueCode = ""; // Declare queueCode here
+
+            // Determine which TextBox is active and append the clicked button's text
+            if (nametxt.Focused)
+            {
+                nametxt.Text += buttonText;
+            }
+            else if (departmentcbx.Focused)
+            {
+                departmentcbx.Text += buttonText;
+            }
+            else if (purposetxt.Focused)
+            {
+                purposetxt.Text += buttonText;
+            }
+
+            // Get today's date
+            DateTime todayDate = DateTime.Today;
+
+            // Insert data into the database
+            string connectionString = "Data Source=reyn\\SQLEXPRESS;Initial Catalog=loginform;Integrated Security=True";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Count the number of records in the database
+                    string countQuery = "SELECT COUNT(*) FROM QueueData";
+                    using (SqlCommand countCommand = new SqlCommand(countQuery, connection))
+                    {
+                        int currentQueueNumber = Convert.ToInt32(countCommand.ExecuteScalar()) + 1;
+
+                        // Format the queue code
+                        queueCode = "A-" + currentQueueNumber.ToString("000");
+
+                        string insertQuery = "INSERT INTO QueueData (Name, Department, Purpose, QueueNumber, QueueDate, QueueCode) VALUES (@Name, @Department, @Purpose, @QueueNumber, @QueueDate, @QueueCode)";
+                        using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@Name", nametxt.Text);
+                            command.Parameters.AddWithValue("@Department", departmentcbx.Text);
+                            command.Parameters.AddWithValue("@Purpose", purposetxt.Text);
+                            command.Parameters.AddWithValue("@QueueNumber", currentQueueNumber);
+                            command.Parameters.AddWithValue("@QueueDate", todayDate);
+                            command.Parameters.AddWithValue("@QueueCode", queueCode);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                // Display a success message
+                MessageBox.Show("Successfully queued with code: " + queueCode, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Optionally, you can clear the input fields after successful insertion.
+                nametxt.Clear();
+                departmentcbx.SelectedIndex = -1;
+                purposetxt.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Handle the error as needed.
+            }
+        }
+    }
+}
